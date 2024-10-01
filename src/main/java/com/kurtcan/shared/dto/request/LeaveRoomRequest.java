3ea3c00ebd690234cbf9c188c653871d703fea25
cccaaannn <can.kurt.aa@gmail.com>
2024-10-02 00:07:18 +0300

    refactor

--- src/main/java/com/kurtcan/shared/dto/request/LeaveRoomRequest.java
+++ src/main/java/com/kurtcan/shared/dto/request/LeaveRoomRequest.java
@@ -1,18 +1,11 @@
 package com.kurtcan.shared.dto.request;
 
-import com.kurtcan.shared.serialization.SerializableField;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class LeaveRoomRequest {
-    @SerializableField
-    private UUID roomId;
 }
