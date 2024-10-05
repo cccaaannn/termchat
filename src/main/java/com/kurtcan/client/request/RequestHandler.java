5066084b46949c97a63698d6ded17e39d659d8b0
cccaaannn <can.kurt.aa@gmail.com>
2024-10-06 01:05:40 +0300

    request handler

---
+++ src/main/java/com/kurtcan/client/request/RequestHandler.java
@@ -0,0 +1,57 @@
+package com.kurtcan.client.request;
+
+import com.kurtcan.client.ClientState;
+import com.kurtcan.shared.threading.Connection;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Console;
+
+@Slf4j
+@SuppressWarnings("InfiniteLoopStatement")
+public class RequestHandler implements Runnable {
+    private final Connection connection;
+    private final Console console = System.console();
+    private final CommandHandler commandHandler;
+
+    public RequestHandler(Connection connection) {
+        this.connection = connection;
+        this.commandHandler = new CommandHandler(connection);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                console.flush();
+                var consoleInput = console.readLine();
+
+                if (!ClientState.isRoomActive()) {
+                    if (CommandHandler.isCommand(consoleInput, CommandHandler.CREATE_ROOM_COMMAND)) {
+                        commandHandler.createRoom(consoleInput);
+                        continue;
+                    }
+                    if (CommandHandler.isCommand(consoleInput, CommandHandler.JOIN_ROOM_COMMAND)) {
+                        commandHandler.joinRoom(consoleInput);
+                        continue;
+                    }
+                }
+
+                if (CommandHandler.isCommand(consoleInput, CommandHandler.LEAVE_ROOM_COMMAND)) {
+                    commandHandler.leaveRoom();
+                    continue;
+                }
+
+                if (ClientState.ACTIVE_ROOM_ID != null) {
+                    commandHandler.messageRoom(consoleInput);
+                    continue;
+                }
+
+                commandHandler.showHelp();
+            }
+
+        } catch (Exception ex) {
+            log.error("Cannot start request handler: {}", ex.getMessage());
+            connection.setAsDisconnected();
+        }
+    }
+}
