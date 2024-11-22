a768cb084faab92fe0153e5f59bba2a4c5598fc3
cccaaannn <can.kurt.aa@gmail.com>
2024-11-23 00:44:47 +0300

    client functions

--- src/main/java/com/kurtcan/client/ClientState.java
+++ src/main/java/com/kurtcan/client/ClientState.java
@@ -1,13 +1,33 @@
 package com.kurtcan.client;
 
+import lombok.Getter;
+
 import java.util.Objects;
 import java.util.UUID;
 
 public class ClientState {
-    public volatile static UUID CLIENT_ID = null;
-    public volatile static UUID ACTIVE_ROOM_ID = null;
+    @Getter
+    private volatile static UUID clientId = null;
+    @Getter
+    private volatile static UUID roomId = null;
 
-    public static boolean isRoomActive() {
-        return Objects.nonNull(ACTIVE_ROOM_ID);
+    public static boolean isMyClient(UUID clientId) {
+        return Objects.equals(ClientState.clientId, clientId);
+    }
+
+    public static boolean isRoomJoined() {
+        return Objects.nonNull(roomId);
+    }
+
+    public static void leaveRoom() {
+        ClientState.roomId = null;
+    }
+
+    public static void joinRoom(UUID roomId) {
+        ClientState.roomId = roomId;
+    }
+
+    public static void connect(UUID clientId) {
+        ClientState.clientId = clientId;
     }
 }
