eea78e23849b1d0aa7459d8bd70db9404710ef45
cccaaannn <can.kurt.aa@gmail.com>
2024-11-24 00:20:06 +0300

    client state functions

--- src/main/java/com/kurtcan/server/client/ClientState.java
+++ src/main/java/com/kurtcan/server/client/ClientState.java
@@ -1,8 +1,28 @@
 package com.kurtcan.server.client;
 
+import lombok.Getter;
+
+import java.util.Objects;
 import java.util.UUID;
 
+@Getter
 public class ClientState {
-    public volatile UUID CLIENT_ID = null;
-    public volatile UUID ROOM_ID = null;
+    private volatile UUID clientId = null;
+    private volatile UUID roomId = null;
+
+    public boolean isRoomJoined() {
+        return Objects.nonNull(this.roomId);
+    }
+
+    public void leaveRoom() {
+        this.roomId = null;
+    }
+
+    public void joinRoom(UUID roomId) {
+        this.roomId = roomId;
+    }
+
+    public void connect(UUID clientId) {
+        this.clientId = clientId;
+    }
 }
